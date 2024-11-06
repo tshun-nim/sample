5a1d9ac61ac95ad437eef8755b04a5f38f118a0e
tshun-nim <tajimas0811@gmail.com>
2024-11-07 02:45:24 +0900

    Initial Commit

---
+++ sample/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sample
+//
+//  Created by 田島隼也 on 2024/11/07.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
